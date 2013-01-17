d40aa8d86461ae3b7d17e027e3400e77109867f7
pablito56 <pablito56@gmail.com>
2013-01-17 10:22:59 +0100

    [FEATURE] Added new and old-style classes, object protocol and super examples

--- example0.py
+++ example0.py
@@ -1,27 +1,34 @@
 #-*- coding: utf-8 -*-
 u'''
-OBJECTS EXAMPLE 0: Everything is an object
+EXAMPLE 0: Objects: type, id and value
 '''
+
+
 # Let's instantiate an int and check its id, type and value
 inst = 12345
 print id(inst)
 print type(inst)
 print inst
 
+
 # Let's compare the ids of two ints
 inst = 12345
 print inst is 12345
-print id(inst) == id(12345)
+lst = [inst]
+print id(inst) == id(lst[0])
+
 
 #===============================================================================
 # - Every object has an identity (its address), a type and a value
 # - Use 'id' and 'is' to retrieve or compare the id of an object
 #===============================================================================
 
+
 # Let's do the same with lists
 inst = []
 print inst is []
 print id(inst) == id([])
+
 
 # WTF!? Let's try again
 inst_int_1 = 12345
@@ -33,18 +40,14 @@ print id(inst_int_1), 'vs.', id(inst_int_2)
 print id(inst_list_1), 'vs.', id(inst_list_2)
 print id(inst_none), 'vs.', id(None)
 
-# Let's try a different thing
-dict1 = {(1, 2): "1, 2", (3, 4): "3, 4"}
-try:
-    dict2 = {[1, 2]: "1, 2", [3, 4]: "3, 4"}
-except TypeError, e:
-    print e.__class__.__name__, e
 
 #===============================================================================
+# - Objects whose value can change are said to be mutable (dicts, lists)
+# - Objects whose value is unchangeable once they are created are called immutable (numbers, strings, tuples)
 # - Mutable types allow in-place modifications (append in a list, pop in a dictionary...)
-# - Immutable types values are reused by the compiler (so their id is the same): int, float, string, tuple
-# - Mutable types are not stable, so they can not be used as dict keys
+# - Immutable types values are reused by the compiler (so their id is the same)
 #===============================================================================
+
 
 # Let's change a string value
 inst = 'instance value'
@@ -52,42 +55,62 @@ print inst, '@', id(inst)
 inst = inst + ' updated'
 print inst, '@', id(inst)
 
+
 # Let's change a list value
 inst = ['instance', 'value']
 print inst, '@', id(inst)
 inst.append('updated')
 print inst, '@', id(inst)
 
+
+# Now let's play with dicts
+dict1 = {(1, 2): "1, 2", (3, 4): "3, 4"}
+try:
+    dict2 = {[1, 2]: "1, 2", [3, 4]: "3, 4"}
+except TypeError, e:
+    print e.__class__.__name__, e
+
+
+#===============================================================================
+# - Mutable types are not stable, so they can not be used as dict keys
+#===============================================================================
+
+
 #===============================================================================
 # This can lead to some common errors!!
-# - Multiple assignment
 #===============================================================================
+
 
 # Multiple assignment with ints (immutable)
 a = b = 0
 print a, '@', id(a)
 print b, '@', id(b)
 
+
 # Let's modify one of the ints
 a += 1
 print a, '@', id(a)
 print b, '@', id(b)
 
-# Multiple assignment with lists (mutable)
+
+# Ok. Multiple assignment with lists (mutable)
 x = y = []
 print x, '@', id(x)
 print y, '@', id(y)
+
 
 # Let's modify one of the lists
 x.extend([1, 2, 3])
 print x, '@', id(x)
 print y, '@', id(y)
 
+
 #===============================================================================
-# This can lead to some common errors!!
-# - Multiple assignment --> Avoid multiple assignment of mutable types
+# Mutable and immutabel types common errors:
+# - Multiple assignment --> Avoid multiple assignment of mutable types!
 # - Class attributes
 #===============================================================================
+
 
 # Use mutable types as class attributes
 class MutablesClass(object):
@@ -96,18 +119,21 @@ class MutablesClass(object):
 inst1 = MutablesClass()
 inst2 = MutablesClass()
 
+
 # Let's change one of the instances of my class
 inst1.list_inst.append(1)
 inst1.int_inst += 1
 print inst2.list_inst
 print inst2.int_inst
 
+
 #===============================================================================
-# This can lead to some common errors!!
+# Mutable and immutabel types common errors:
 # - Multiple assignment --> Avoid multiple assignment of mutable types
-# - Class attributes --> Instantiate in the __init__
+# - Class attributes --> Instantiate in the __init__ !
 # - Functions parameters default value
 #===============================================================================
+
 
 # Let's create and call a method
 def add_to_list(item, lst=[]):
@@ -118,9 +144,10 @@ result2 = add_to_list(2)
 print result1 is result2
 print result1, 'vs', result2
 
+
 #===============================================================================
-# This can lead to some common errors!!
+# Mutable and immutabel types common errors:
 # - Multiple assignment --> Avoid multiple assignment of mutable types
 # - Class attributes --> Instantiate in the __init__
-# - Functions parameters default value --> Use None as default value and instantiate inside the function 
+# - Functions parameters default value --> Use None as default value and instantiate inside the function ! 
 #===============================================================================
---
+++ example1.py
@@ -0,0 +1,136 @@
+#-*- coding: utf-8 -*-
+u'''
+EXAMPLE 1: Classes
+'''
+
+
+# Let's create an old-style class
+class MyOldClass:
+    def print_instance_class(self):
+        print type(self)
+        print self.__class__
+
+old_inst = MyOldClass()
+old_inst.print_instance_class()
+
+
+# Let's create a new-style class
+class MyNewClass(object):
+    def print_instance_class(self):
+        print type(self)
+        print self.__class__
+
+new_inst = MyNewClass()
+new_inst.print_instance_class()
+
+
+#===============================================================================
+# - New-style classes introduced in 2.2 to unify classes and types
+# - Provides unified object model with a full meta-model
+# - Other benefits: subclass most built-in types, descriptors (slots, properties, static and class methods)...
+# - By default all classes are old-style until Python 3
+#
+# - Other changes introduced: __new__, new dir() behavior, metaclasses, new MRO (also in 2.3)
+#
+# - More info: http://www.python.org/doc/newstyle/
+#===============================================================================
+
+
+# Let's customize our classes
+class MyFraction(object):
+    def __init__(self, numerator, denominator):
+        self.num = int(numerator)
+        self.den = int(denominator)
+
+    def __str__(self):
+        return "{0} / {1}".format(self.num, self.den)
+
+    def __repr__(self):
+        return "{0}({1}, {2})".format(self.__class__.__name__, self.num, self.den)
+
+    def value(self):
+        return float(self.num) / self.den
+
+    def __lt__(self, other):
+        try:
+            return self.value() < other.value()
+        except AttributeError:
+            return self.value() < other
+
+    def __le__(self, other):
+        try:
+            return self.value() <= other.value()
+        except AttributeError:
+            return self.value() <= other
+
+    def __eq__(self, other):
+        try:
+            return self.value() == other.value()
+        except AttributeError:
+            return self.value() == other
+
+    def __ne__(self, other):
+        try:
+            return self.value() != other.value()
+        except AttributeError:
+            return self.value() != other
+
+    def __gt__(self, other):
+        try:
+            return self.value() > other.value()
+        except AttributeError:
+            return self.value() > other
+
+    def __ge__(self, other):
+        try:
+            return self.value() >= other.value()
+        except AttributeError:
+            return self.value() >= other
+
+    def __getitem__(self, key):
+        if key == 0 or key == 'num':
+            return self.num
+        elif key == 1 or key == 'den':
+            return self.den
+        else:
+            raise KeyError(key)
+
+
+f1 = MyFraction(4, 2)
+f2 = MyFraction(3, 2)
+
+
+print f1
+print repr(f2)
+
+
+print f1 == 2
+print f2 <= f1
+
+
+print f1['num'], "/", f1[1], "=", f1
+
+
+class MirrorFloat(float):
+    def __new__(cls, val=0):
+        inst = float.__new__(cls, 1.0/val)
+        inst.val = val
+        return inst
+
+    def __repr__(self):
+        return "{0}({1})".format(self.__class__.__name__, self.val)
+
+    def __add__(self, other):
+        return float.__sub__(self, other)
+
+    def __sub__(self, other):
+        return float.__add__(self, other)
+
+
+inst = MirrorFloat(-7)
+print inst
+print repr(inst)
+
+
+print inst + 2
+print inst - 5
